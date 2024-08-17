b0740d48b814ebbbb95b29c66bbbe4c549b1bce1
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2024-08-17 13:29:26 +0800

    add index.js (testing only)

---
+++ index.js
@@ -0,0 +1,20 @@
+const Express = require("express");
+const app = Express();
+const PORT = 3000;
+
+const { Server } = require('socket.io');
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('disconnect', () => console.log('A user disconnected'));
+});
+
+app.post("/donation-callback", (req, res) => {
+  console.log(req.body);
+});
+
+const server = app.listen(PORT, () => {
+  console.log(`API listened on ${PORT}`);
+});
