00ee96eb4b4a49aae8fae97909844220fe2ab12f
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2024-08-17 16:35:26 +0800

    allow Authorization on CORS header

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(Express.json({type: "application/json"}));
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept", "Authorization");
 
   next();
 });
